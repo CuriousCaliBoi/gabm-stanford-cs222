39ebefac0b4b59f58413de81058a6c8e7106f564
KingZuko <rs6121@nyu.edu>
2024-10-15 12:33:12 -0700

    added the openai api key and owner name

--- simulation_engine/settings.py
+++ simulation_engine/settings.py
@@ -2,8 +2,8 @@ import sys
 import os
 from pathlib import Path
 
-OPENAI_API_KEY = "[Fill in: Your OpenAI API Key]"
-KEY_OWNER = "[Fill in: Your name]"
+OPENAI_API_KEY ="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"
+KEY_OWNER = "Rohan Saxena"
 DEBUG = True
 MAX_CHUNK_SIZE = 4
 LLM_VERS = "gpt-4o-mini"
